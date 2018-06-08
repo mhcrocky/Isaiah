1db0839feeb38c4d6ce0f4097911977b0991eab1
es9d <larastar721@gmail.com>
2018-06-08 02:42:16 -0700

    Adding backbone and mustache requirements.

--- app/config/database.php
+++ app/config/database.php
@@ -54,13 +54,13 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
+			'host'      => '127.0.0.1',
 			'database'  => 'isaiahde_logos',
 			'username'  => 'isaiahde_tekton',
 			'password'  => 'kkg7n5a4',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
-			'prefix'    => 'laravel',
+			'prefix'    => '',
 		),
 
 		'pgsql' => array(
