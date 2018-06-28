28788a1979c5e5b559174b18ebcbfa32f929552d
es9d <larastar721@gmail.com>
2018-06-28 02:33:31 -0700

    Adding Boris REPL to PHP Composer and Laravel, for commandline REPL with Laravel framework. Usage (from Laravel root e.g. /home/vagrant/Code/Laravel or /public_html/dev-isaiah)> php artisan boris

--- app/config/app.php
+++ app/config/app.php
@@ -123,6 +123,7 @@ return array(
 		'Illuminate\Workbench\WorkbenchServiceProvider',
         'Way\Generators\GeneratorsServiceProvider',
         'Conarwelsh\MustacheL4\MustacheL4ServiceProvider',
+        'DaveJamesMiller\Boris\BorisServiceProvider',
 	),
 
 	/*
