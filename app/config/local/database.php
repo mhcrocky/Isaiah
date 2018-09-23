ea5e1961e8df89e964456b3756fbef99caec4e8c
es9d <larastar721@gmail.com>
2018-09-23 16:09:33 -0700

    Setting local laraval database to use isaiahde_logos schema.

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,7 +23,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'homestead',
+			'database'  => 'isaiahde_logos',
 			'username'  => 'homestead',
 			'password'  => 'secret',
 			'charset'   => 'utf8',
