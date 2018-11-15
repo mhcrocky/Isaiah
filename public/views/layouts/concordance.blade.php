0d2b418d219e5698111eff28260398d62eb72cc8
es9d <larastar721@gmail.com>
2018-11-15 13:22:05 -0800

    Updating Concordance template to match changes made in the master layout template.

--- public/views/layouts/concordance.blade.php
+++ public/views/layouts/concordance.blade.php
@@ -47,9 +47,18 @@
                 </div>
                 <div class="navbar-collapse collapse navbar-right">
                     <ul class="nav navbar-nav">
-                        <li><a href="http://dev.isaiahexplained.com/"><i class="fa fa-home fa-fw"></i> Back to Home</a></li>
-                        <li><a href="/"><i class="fa fa-book fa-fw"></i> Chapter Index</a></li>
-                        <li><a href="/concordance/"><i class="fa fa-list fa-fw"></i> Concordance Index</a></li>
+                        <li class="dropdown"><a title="<i class=&quot;fa fa-graduation-cap fa-fw&quot;></i>&nbsp;<span>Resources</span>" href="#" data-toggle="dropdown" class="dropdown-toggle" aria-haspopup="true"><i class="fa fa-graduation-cap fa-fw"></i>&nbsp;<span>Resources</span> <span class="caret"></span></a>
+                            <ul role="menu" class="dropdown-menu">
+                                <li><a title="Key Features of the Prophecy of Isaiah" href="/resources/key-features-of-the-prophecy-of-isaiah/">Key Features of the Prophecy of Isaiah</a></li>
+                                <li><a title="Isaiah’s Layered Literary Structures" href="/resources/isaiahs-layered-literary-structures/">Isaiah’s Layered Literary Structures</a></li>
+                                <li><a title="Isaiah’s Seven Spiritual Levels of Humanity" href="/resources/isaiahs-seven-spiritual-levels-of-humanity/">Isaiah’s Seven Spiritual Levels of Humanity</a></li>
+                                <li><a title="Isaiah’s Ancient Types of End-Time Events" href="/resources/isaiahs-ancient-types-of-end-time-events/">Isaiah’s Ancient Types of End-Time Events</a></li>
+                                <li><a title="Overviews of the Prophecy of Isaiah" href="/resources/overviews-of-the-prophecy-of-isaiah/">Overviews of the Prophecy of Isaiah</a></li>
+                                <li><a title="Glossary of Terms Relating to Isaiah" href="/resources/glossary-of-terms-relating-to-isaiah/">Glossary of Terms Relating to Isaiah</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="/blog/"><i class="fa fa-rss fa-fw"></i> Blog</a></li>
+                        <li><a href="/blog/contact/"><i class="fa fa-envelope fa-fw"></i> Contact</a></li>
                         <li class="show-hide nav-search">
                             {{ $mobile_search or '' }}
                         </li>
@@ -76,32 +85,42 @@
                     <div class="col-xs-12 col-sm-3 col-list">
                         <h3>Isaiah Explained</h3>
                         <ul>
-                            <li><a href="">Home</a></li>
-                            <li><a href="">Resources</a></li>
-                            <li><a href="">About</a></li>
-                            <li><a href="">Contact</a></li>
+                            <li><a href="/">Home</a></li>
+                            <li class="dropdown"><a title="<i class=&quot;fa fa-graduation-cap fa-fw&quot;></i>&nbsp;<span>Resources</span>" href="#" data-toggle="dropdown" class="dropdown-toggle" aria-haspopup="true"><i class="fa fa-graduation-cap fa-fw"></i>&nbsp;<span>Resources</span> <span class="caret"></span></a>
+                                <ul role="menu" class="dropdown-menu">
+                                    <li><a title="Key Features of the Prophecy of Isaiah" href="/resources/key-features-of-the-prophecy-of-isaiah/">Key Features of the Prophecy of Isaiah</a></li>
+                                    <li><a title="Isaiah’s Layered Literary Structures" href="/resources/isaiahs-layered-literary-structures/">Isaiah’s Layered Literary Structures</a></li>
+                                    <li><a title="Isaiah’s Seven Spiritual Levels of Humanity" href="/resources/isaiahs-seven-spiritual-levels-of-humanity/">Isaiah’s Seven Spiritual Levels of Humanity</a></li>
+                                    <li><a title="Isaiah’s Ancient Types of End-Time Events" href="/resources/isaiahs-ancient-types-of-end-time-events/">Isaiah’s Ancient Types of End-Time Events</a></li>
+                                    <li><a title="Overviews of the Prophecy of Isaiah" href="/resources/overviews-of-the-prophecy-of-isaiah/">Overviews of the Prophecy of Isaiah</a></li>
+                                    <li><a title="Glossary of Terms Relating to Isaiah" href="/resources/glossary-of-terms-relating-to-isaiah/">Glossary of Terms Relating to Isaiah</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="/About">About</a></li>
+                            <li><a href="/blog/contact/">Contact</a></li>
                         </ul>
                     </div>
                     <div class="col-xs-12 col-sm-3 col-list">
                         <h3>Network</h3>
                         <ul>
-                            <li><a href="//josephandjudah.com" target="_blank">JosephAndJudah.com</a></li>
+                            <!--<li><a href="//josephandjudah.com" target="_blank">JosephAndJudah.com</a></li>-->
                             <li><a href="//isaiahreport.com" target="_blank">IsaiahReport.com</a></li>
                             <li><a href="//isaiahprophecy.com" target="_blank">IsaiahProphecy.com</a></li>
                         </ul>
                     </div>
                     <div class="col-xs-12 col-sm-3 col-list">
                         <h3>Read Isaiah</h3>
-                        <ul>
-                            <li><a href="/">Isaiah Chapter Index</a></li>
-                            <li><a href="/search/">Search Isaiah</a></li>
-                            <li><a href="/concordance/">Isaiah Concordance Index</a></li>
+                        <ul class="nav-read-isaiah">
+                            <li><a href="/1#one_col" title="Isaiah Institute Translation"><i class="fa fa-align-justify fa-fw"></i><span>Isaiah Institute Translation</span></a></li>
+                            <li><a href="/1#three_col" title="Comparative Translation"><i class="fa fa-table fa-fw"></i><span>Comparative Translation</span></a></li>
+                            <li><a href="/1#commentary" title="Apocalyptic Commentary"><i class="fa fa-quote-left fa-fw"></i><span>Apocalyptic Commentary</span></a></li>
+                            <li><a href="/1#concordance" title="Interactive Concordance"><i class="fa fa-list fa-fw"></i><span>Interactive Concordance</span></a></li>
                         </ul>
                     </div>
                     <div class="col-xs-12 col-sm-3">
                         <h3>Social</h3>
                         <ul>
-                            <li><a class="btn btn-facebook" href=""><i class="fa fa-facebook fa-fw"></i>&nbsp;&nbsp;Facebook</a></li>
+                            <!--<li><a class="btn btn-facebook" href=""><i class="fa fa-facebook fa-fw"></i>&nbsp;&nbsp;Facebook</a></li>-->
                             <!--<li><a class="btn btn-twitter" href=""><i class="fa fa-twitter fa-fw"></i>&nbsp;&nbsp;twitter</a></li>-->
                         </ul>
                     </div>
