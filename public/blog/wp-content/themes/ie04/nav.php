7301b018c13576dc3fbefce53d141056c0b094eb
es9d <larastar721@gmail.com>
2018-12-02 00:42:24 -0800

    Adding nav.php to fix issue debugging locally.

---
+++ public/blog/wp-content/themes/ie04/nav.php
@@ -0,0 +1,29 @@
+<div id="main-nav" class="navbar-collapse collapse navbar-right">
+  <ul class="nav navbar-nav">
+    <li><a title="Home" href="//dev-isaiah.isaiahexplained.com/"><i class="fa fa-home fa-fw"></i>&nbsp;<span>Home</span></a></li>
+    <li class="dropdown"><a title="<i class=&quot;fa fa-graduation-cap fa-fw&quot;></i>&nbsp;<span>Resources</span>" href="#" data-toggle="dropdown" class="dropdown-toggle" aria-haspopup="true"><i class="fa fa-graduation-cap fa-fw"></i>&nbsp;<span>Resources</span> <span class="caret"></span></a>
+      <ul role="menu" class="dropdown-menu">
+        <li><a title="Key Features of the Prophecy of Isaiah" href="/resources/key-features-of-the-prophecy-of-isaiah/">Key Features of the Prophecy of Isaiah</a></li>
+        <li><a title="Isaiah's Layered Literary Structures" href="/resources/isaiahs-layered-literary-structures/">Isaiah's Layered Literary Structures</a></li>
+        <li><a title="Isaiah's Seven Spiritual Levels of Humanity" href="/resources/isaiahs-seven-spiritual-levels-of-humanity/">Isaiah's Seven Spiritual Levels of Humanity</a></li>
+        <li><a title="Isaiah's Ancient Types of End-Time Events" href="/resources/isaiahs-ancient-types-of-end-time-events/">Isaiah's Ancient Types of End-Time Events</a></li>
+        <li><a title="Overviews of the Prophecy of Isaiah" href="/resources/overviews-of-the-prophecy-of-isaiah/">Overviews of the Prophecy of Isaiah</a></li>
+        <li><a title="Glossary of Terms Relating to Isaiah" href="/resources/glossary-of-terms-relating-to-isaiah/">Glossary of Terms Relating to Isaiah</a></li>
+        <li><a title="KJV Bible" href="/bible">KJV Bible</a></li>
+      </ul>
+     </li>
+     <li><a href="#"><i class="fa fa-shopping-cart fa-fw"></i> <span>Store</span></a></li>
+     <li><a href="//isaiahexplained.com/blog/contact/"><i class="fa fa-envelope fa-fw"></i> <span>Contact</span></a></li>
+     <li class="show-hide nav-search">
+       <form role="search">
+				<div class="input-group">
+				<input name="search-box" type="text" class="form-control search-field" placeholder="Search Isaiah" value="nation">
+				<span class="input-group-btn">
+				<button class="btn btn-warning" type="submit"><i class="fa fa-search"></i></button>
+				</span>
+				<span name="search-error"></span>
+				</div><!-- /search -->
+			</form>
+     </li>
+  </ul>
+</div>
