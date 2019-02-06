b71a8edc25bada9cf6edb075ddaf8982a9a1ca19
es9d <larastar721@gmail.com>
2019-02-06 12:23:57 -0800

    Fixes for redirecting legacy content.

--- app/repositories/RedirectRepository.php
+++ app/repositories/RedirectRepository.php
@@ -16,7 +16,7 @@ class RedirectRepository {
             $h = '';
         }
 
-        if(preg_match('/\/isaiah_ch_(\d{1,2})\.html/', $uri, $matches)) {
+        if(preg_match('/^\/isaiah_ch_(\d{1,2})\.html/', $uri, $matches)) {
             if(!empty($matches[1])) {
                 $chapter_number = ltrim($matches[1], '0');
                 $redirect_url = "/{$chapter_number}#one_col";
@@ -37,7 +37,7 @@ class RedirectRepository {
                     $redirect_url = "/{$chapter_number}?citation=c{$chapter_number}v{$verse_number}-{$word}#concordance";
                 }
             }
-        } elseif(preg_match('/\/Concordance\/concordance([A-Z])\.html/i', $uri, $matches)) {
+        } elseif(preg_match('/^\/Concordance\/concordance([A-Z])\.html/i', $uri, $matches)) {
             if(!empty($matches[1])) {
                 $concordance_letter = $matches[1];
                 if(preg_match('/c(\d{1,2})v(\d{1,2})(a|b)?-(\w+)/', $h, $h_matches)) {
@@ -54,21 +54,45 @@ class RedirectRepository {
                     $redirect_url = "/concordance/{$concordance_letter}?citation=c{$chapter_number}v{$verse_number}-{$word}#{$word}";
                 }
             }
-        } elseif(preg_match('/\/Concordance\/title\.html/i', $uri)) {
+        } elseif(preg_match('/^\/Concordance\/title\.html/i', $uri)) {
             $redirect_url = '/Isaiah-Institute-Translation/';
-        } elseif(preg_match('/\/Concordance\/index\.html/i', $uri)) {
+        } elseif(preg_match('/^\/Concordance\/index\.html/i', $uri)) {
             $redirect_url = '/Concordance/';
         } else {
-            $filename = $_SERVER['HOME'] . '/public_html/isaiahexplained_legacy' . $uri;
+            $new_path = '/legacy' . urldecode($uri);
+            $filename = public_path() . '/legacy' . urldecode($uri);
+            //dd($filename);
             if(file_exists($filename)) {
-                $filesize = filesize($filename);
-                $fh = fopen($filename, 'r');
-                $html = fread($fh, $filesize);
-                echo $html;
-                exit;
+                /*if(ends_with($filename, 'html')) {
+                    $filesize = filesize($filename);
+                    $fh = fopen($filename, 'r');
+                    $html = fread($fh, $filesize);
+                    $html = str_replace('"style.css"', '/legacy/style.css', $html);
+                    $html = str_replace('../', '/legacy/', $html);
+                    echo $html;*/
+                    /*$doc = new DOMDocument();
+                    libxml_use_internal_errors(true);
+                    $doc->loadHTMLFile($filename);
+                    echo $doc->saveHTML();*/
+                    /*exit;
+                } else {
+                    $redirect_url = $new_path;
+                }*/
+                $redirect_url = $new_path;
             }
+            //dd($filename);
         }
 
         return $redirect_url;
     }
+
+    /*private function endsWith($haystack, $needle)
+    {
+        $length = strlen($needle);
+        if ($length == 0) {
+            return true;
+        }
+
+        return (substr($haystack, -$length) === $needle);
+    }*/
 }
