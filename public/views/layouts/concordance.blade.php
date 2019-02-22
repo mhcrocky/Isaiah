d23fdc9e1b3e7a7014743398c6b4c83333fe7b86
es9d <larastar721@gmail.com>
2019-02-22 01:11:25 -0800

    Fix for site tracking JS code missing due to erroneous environment checking. Also added favicon (finally whoops)

--- public/views/layouts/concordance.blade.php
+++ public/views/layouts/concordance.blade.php
@@ -8,6 +8,8 @@
     {{ $meta or '' }}
     <title>{{ $title }}</title>
 
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon" />
     <!-- Stylesheets -->
     <!-- FontAwesome 4.1.0 -->
     <link href="http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" />
@@ -31,7 +33,7 @@
     <link href="http://fonts.googleapis.com/css?family=Roboto+Slab:400,300,700,100|Roboto+Condensed:300italic,400italic,700italic,400,300,700|Roboto:400,100,100italic,300,300italic,500,500italic,400italic,700,700italic,900,900italic" rel="stylesheet" type="text/css">
     <!-- End Stylesheets -->
 
-    @if ($app_url == 'http://isaiahexplained.com')
+    @if (!App::environment('local', 'staging'))
         {{ $tracking_code }}
     @endif
 
