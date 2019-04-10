0217d980470ba19098e394b66d354bdbbef3e7e3
es9d <larastar721@gmail.com>
2019-04-09 22:37:32 -0700

    Testing POST deployment hook with dev.isaiahexplained.com.

--- app/controllers/ResourceController.php
+++ app/controllers/ResourceController.php
@@ -110,7 +110,7 @@ class ResourceController extends BaseController {
         // parse the desired JSON data into HTML for use on your site
         $comments = $results->response;
 
-        //Testing deploy log 2
+        //Testing deploy log 3
         //foreach($comments as $comment) {
             //dd($comment->message);
         //}
