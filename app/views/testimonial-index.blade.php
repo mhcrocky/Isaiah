38ae5a0dfcacca793a50a6dea190ef77bd9d8f82
es9d <larastar721@gmail.com>
2019-04-10 01:18:43 -0700

    Adding disqus JS widget to try and grap api_key from AJAX, since there appears to be no other way

--- app/views/testimonial-index.blade.php
+++ app/views/testimonial-index.blade.php
@@ -24,6 +24,20 @@
         @endforeach
     @endif
 
+    <div id="disqus_thread"></div>
+    <script type="text/javascript">
+        /* * * CONFIGURATION VARIABLES * * */
+        var disqus_shortname = 'isaiah-explained';
+
+        /* * * DON'T EDIT BELOW THIS LINE * * */
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+
     <h2 class="title-chapters">Submit Your Testimonial</h2>
 
     {{ Form::open(array('action' => 'TestimonialController@SubmitTestimonialForm')) }}
