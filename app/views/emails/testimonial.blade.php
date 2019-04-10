cd9b27ebf6ff8a473e6af726cfa3ba6798d41178
es9d <larastar721@gmail.com>
2019-04-10 02:07:05 -0700

    Updating Disqus url in testimonial notification email. Would like to have this set to a page just for the comment, but there's really not even a working anchor url that I'm seeing for that. Oh well.

--- app/views/emails/testimonial.blade.php
+++ app/views/emails/testimonial.blade.php
@@ -4,4 +4,4 @@
 <p>{{$phone_number}}</p>
 <p>Testimonial (preview):</p>
 <p>&ldquo;{{$body}}&rdquo;&mdash;{{$full_name}}</p>
-<p><a href="https://disqus.com/">Click to moderate on Disqus.com</a></p>
+<p><a href="https://isaiah-explained.disqus.com/admin/moderate/#/pending">Click to moderate on Disqus.com</a></p>
