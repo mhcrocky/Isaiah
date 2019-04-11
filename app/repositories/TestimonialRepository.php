e24699f61c1f7ba2ae8d7674b4c6aeaa822f4f2b
es9d <larastar721@gmail.com>
2019-04-10 18:10:43 -0700

    Adding name citation template to enable more than simple Display Name when displaying testimonials (so we can import the legacy testimonials that are no longer available on the index page).

--- app/repositories/TestimonialRepository.php
+++ app/repositories/TestimonialRepository.php
@@ -32,7 +32,10 @@ class TestimonialRepository {
             $message = $comments[$i]->message;
             $message = str_ireplace('<p>', '', $message);
             $message = str_ireplace('</p>', '', $message);
+            preg_match_all('###(.+?)###', $message, $m);
+            $citation = $m[1];
             $comments[$i]->message = $message;
+            $comments[$i]->author->name = $citation;
             //dd(htmlentities($comment->message));
         }
 
