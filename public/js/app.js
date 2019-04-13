4e5919f0bffba5eb0aa502974d086768cfc10d1f
es9d <larastar721@gmail.com>
2019-04-13 04:48:50 -0700

    Fixing testimonial ajax form CSRF token.

--- public/js/app.js
+++ public/js/app.js
@@ -172,7 +172,8 @@ if (location.hash || location.pathname.match(/\/\d{1,2}/)) {
                         $testimonialContainer.append('<div class="dsq-widget-comment"><p class="dsq-comment-content">&ldquo;' + this.message + '&rdquo;&mdash;' + this.author.name + '</p>');
                     });
 
-                    $('#csrf-token').val(data.token);
+                    $("meta[name='csrf-token']").remove();
+                    $('head').append( '<meta name="csrf-token" content="' + data.token + '">' );
                     $('#disqus-prev').val(data.prev);
                     $('#disqus-next').val(data.next);
                 }
