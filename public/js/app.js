e885b8188e6351278cee357e7719aac4dc6c0c09
es9d <larastar721@gmail.com>
2019-04-13 03:43:49 -0700

    Testing testimonial ajax form JS.

--- public/js/app.js
+++ public/js/app.js
@@ -165,8 +165,8 @@ if (location.hash || location.pathname.match(/\/\d{1,2}/)) {
                         $testimonialContainer.append('<div class="dsq-widget-comment"><p class="dsq-comment-content">&ldquo;' + this.message + '&rdquo;&mdash;' + this.author.name + '</p>');
                     });
                 } //success
-                var test = 1;
             }); //done
+            var test = 1;
         });
 
         $('#nav-left-disqus').on('click', function (e) {
