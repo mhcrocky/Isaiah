313ab37626799398f2e75bac1c6dac6e680a7060
es9d <larastar721@gmail.com>
2019-04-13 04:30:51 -0700

    Fixing testimonial pager form _token selector.

--- public/js/app.js
+++ public/js/app.js
@@ -167,7 +167,7 @@ if (location.hash || location.pathname.match(/\/\d{1,2}/)) {
                         $testimonialContainer.append('<div class="dsq-widget-comment"><p class="dsq-comment-content">&ldquo;' + this.message + '&rdquo;&mdash;' + this.author.name + '</p>');
                     });
 
-                    $("input[name='_token']").val(data.token);
+                    $("[name='_token']").val(data.token);
                     $('#disqus-prev').val(data.prev);
                     $('#disqus-next').val(data.next);
                 }
