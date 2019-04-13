7c7328ed5627877144cb22fec4c4832b9abbf576
es9d <larastar721@gmail.com>
2019-04-13 04:43:58 -0700

    Fixing template form CSRF tag.

--- public/views/layouts/concordance.blade.php
+++ public/views/layouts/concordance.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta id="csrf-token" name="csrf-token" content="{{ csrf_token() }}">
     {{ $robot_meta or '' }}
     {{ $meta or '' }}
     <title>{{ $title }}</title>
